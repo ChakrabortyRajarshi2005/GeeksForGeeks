e34cac17c44af6aa7bef067f802350ed1440bf9a
Rajarshi Chakraborty <rajarshi29032005@gmail.com>
2025-08-30 23:16:40 +0530

    Create The Celebrity Problem.cpp

---
+++ The Celebrity Problem.cpp
@@ -0,0 +1,61 @@
+The Celebrity Problem
+Difficulty: MediumAccuracy: 38.33%Submissions: 342K+Points: 4Average Time: 30m
+A celebrity is a person who is known to all but does not know anyone at a party. A party is being organized by some people. A square matrix mat[][] of size n*n is used to represent people at the party such that if an element of row i and column j is set to 1 it means ith person knows jth person. You need to return the index of the celebrity in the party, if the celebrity does not exist, return -1.
+
+Note: Follow 0-based indexing.
+
+Examples:
+
+Input: mat[][] = [[1, 1, 0],
+                [0, 1, 0],
+                [0, 1, 1]]
+Output: 1
+Explanation: 0th and 2nd person both know 1st person and 1st person does not know anyone. Therefore, 1 is the celebrity person.
+Input: mat[][] = [[1, 1], 
+                [1, 1]]
+Output: -1
+Explanation: Since both the people at the party know each other. Hence none of them is a celebrity person.
+Input: mat[][] = [[1]]
+Output: 0
+Constraints:
+1 ≤ mat.size() ≤ 1000
+0 ≤ mat[i][j] ≤ 1
+mat[i][i] = 1
+
+
+class Solution {
+  public:
+    int celebrity(vector<vector<int>>& mat) {
+        int n = mat.size();
+
+        int i = 0, j = n - 1;
+        while (i < j) {
+
+            // j knows i, thus j can't be celebrity
+            if (mat[j][i] == 1)
+                j--;
+
+            // else i can't be celebrity
+            else
+                i++;
+        }
+
+        // i points to our celebrity candidate
+        int c = i;
+
+        // Check if c is actually
+        // a celebrity or not
+        for (i = 0; i < n; i++) {
+            if (i == c)
+                continue;
+
+            // If any person doesn't
+            // know 'c' or 'c' doesn't
+            // know any person, return -1
+            if (mat[c][i] || !mat[i][c])
+                return -1;
+        }
+
+        return c;
+    }
+};
